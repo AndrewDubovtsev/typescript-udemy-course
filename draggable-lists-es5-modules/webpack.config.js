ca403c072eeb3c7b5b9440549c822a9ecec32e28
Andrew Dubovtsev <andrew.dubovtsev@viasat.com>
2020-05-15 09:59:27 +0100

    added comments to webpack configs

--- draggable-lists-es5-modules/webpack.config.js
+++ draggable-lists-es5-modules/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
-
+// Exporting a module in a Node JS environment
 module.exports = {
     mode: 'development',
+    // An entry point for the entire application
+    // Webpack will go to that file and look up for the imports used in that file,
+    // then it will go to those files until it knows all the files which are used in the project
     entry: './src/app.ts',
     output: {
+        // A single JS file which will be produced in the end
         filename: 'bundle.js',
-        // Constructs an absolute path to the dist folder
+        // Resolve method constructs an absolute path to the output dist folder
         path: path.resolve(__dirname, 'dist'),
         // Informing webpack dev server where to find our bundle.js
         publicPath: 'dist'
@@ -13,18 +17,25 @@ module.exports = {
     // For using source maps (to view TypeScript files in a browser)
     devtool: 'inline-source-map',
     module: {
+        // We define rules to tell Webpack how it should handle files with different extensions
         rules: [
             {
+                // Test describes a test Webpack will perform for any file it finds to determine
+                // whether this rule applies to that file
                 // Any file with an extension ".ts" should be handled by this rule
                 test: /\.ts$/,
                 // Specifying a loader which webpack should use
+                // A loader is a package which helps us handle files with certain extensions
                 use: 'ts-loader',
                 exclude: /node_modules/
             }
         ]
     },
+    // By default Webpack will bundle all JavaScript files, but we want it to bundle
+    // TypeScript files as we;;
     resolve: {
         // Webpack will bundle all the files with these extensions
+        // and put them together
         extensions: ['.ts', '.js']
     }
 };
--- draggable-lists-es5-modules/webpack.config.prod.js
+++ draggable-lists-es5-modules/webpack.config.prod.js
@@ -2,13 +2,16 @@ const path = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    // Production mode will optimise our code to make it as short as possible
     mode: 'production',
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
         // Constructs an absolute path to the dist folder
         path: path.resolve(__dirname, 'dist'),
+        // We don't specify publicPath here, because we don't use webpack dev server
     },
+    // We don't need to use source maps here
     devtool: 'none',
     module: {
         rules: [
@@ -27,6 +30,7 @@ module.exports = {
     },
     plugins: [
         // It will clear everything in the dist folder
+        // before the new output is written
         new CleanPlugin.CleanWebpackPlugin()
     ]
 };
